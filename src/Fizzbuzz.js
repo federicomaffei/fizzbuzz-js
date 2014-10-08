9da57b4b4f62bd622768dcf4b764e454a0e9224e
Roi Driscoll <roi.driscoll@gmail.com>
2014-10-08 15:50:47 +0100

    Added test for main gameplay

--- src/Fizzbuzz.js
+++ src/Fizzbuzz.js
@@ -1,13 +1,17 @@
 function Fizzbuzz() {};
 
 Fizzbuzz.prototype.isDivisibleByThree = function(number) {
-	return (number % 3 === 0);
+  return this._isDivisibleBy(number, 3);
 };
 
 Fizzbuzz.prototype.isDivisibleByFive = function(number){
-  return (number % 5 === 0);
+  return this._isDivisibleBy(number, 5);
 };
 
 Fizzbuzz.prototype.isDivisibleByFifteen = function(number){
-  return (number % 15 === 0);
+  return this._isDivisibleBy(number, 15);
+};
+
+Fizzbuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
 };
